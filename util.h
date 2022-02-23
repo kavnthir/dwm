3034a65660624837ba54409deee832f19f441bf8
kavin <kavnthir@vt.edu>
2022-02-23 01:46:38 -0500

    refactor shmcd() helpter

--- util.h
+++ util.h
@@ -4,5 +4,8 @@
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
